9af7ffe76415ce7c258052ac726d26eb9aefdacf
Mihály Orsós <ormraat.pte@gmail.com>
2026-04-20 19:40:44 +0200

    fix4

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,5 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  vite: {
-    base: process.env.GITHUB_PAGES ? "/womenandai/" : "/",
-  },
+  base: "/womenandai/",
 });
