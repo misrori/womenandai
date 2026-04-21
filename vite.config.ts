8d33ad113613422f4a769636abbe77d25e2f941d
Mihály Orsós <ormraat.pte@gmail.com>
2026-04-21 07:59:58 +0200

    custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/womenandai/",
+  base: "/",
   plugins: [
     react(),
     tsconfigPaths(),
